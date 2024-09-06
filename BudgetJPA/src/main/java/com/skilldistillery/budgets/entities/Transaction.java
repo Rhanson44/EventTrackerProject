3666823393a6c0dc8a489aba411388311eaf9261
Ryan Hanson <hansonrj8322@gmail.com>
2024-09-06 15:50:20 -0600

    Create transaction entity, test mapping

---
+++ BudgetJPA/src/main/java/com/skilldistillery/budgets/entities/Transaction.java
@@ -0,0 +1,97 @@
+package com.skilldistillery.budgets.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Transaction {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String type;
+	private Double amount;
+	private String description;
+	
+	@Column(name = "payment_date")
+	private LocalDateTime paymentDate;
+	
+//	@ManyToOne
+//	@JoinColumn(name = "category_id")
+//	private Category category;
+	
+	public Transaction() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(LocalDateTime paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transaction other = (Transaction) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaction [id=" + id + ", type=" + type + ", amount=" + amount + ", description=" + description
+				+ ", paymentDate=" + paymentDate + "]";
+	}
+	
+}
---
+++ BudgetJPA/src/test/java/com/skilldistillery/budgets/entities/TransactionTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.budgets.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class TransactionTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Transaction transaction;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("BudgetJPA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		transaction = em.find(Transaction.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		transaction = null;
+	}
+
+	@Test
+	void test_Transaction_entity_mapping() {
+		assertNotNull(transaction);
+		assertEquals("Income", transaction.getType());
+	}
+
+}
