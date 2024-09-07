c7a9075286cb170cba082e346a921825d0c1d98e
Ryan Hanson <hansonrj8322@gmail.com>
2024-09-07 08:12:40 -0600

    Create endpoints for transaction entity

--- BudgetTracker/src/main/java/com/skilldistillery/budgets/controllers/TransactionController.java
+++ BudgetTracker/src/main/java/com/skilldistillery/budgets/controllers/TransactionController.java
@@ -3,8 +3,12 @@ package com.skilldistillery.budgets.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,23 @@ public class TransactionController {
 	@GetMapping("transactions/{id}")
 	public Transaction findById(@PathVariable("id") int id) {
 		return service.show(id);
+	}
+	
+	@PostMapping("transactions")
+	public Transaction create(@RequestBody Transaction transaction) {
+		return service.create(transaction);
+	}
+	
+	@PutMapping("transactions/{id}")
+	public Transaction update(@RequestBody Transaction transaction, @PathVariable("id") int id) {
+		return service.update(id, transaction);
+	}
+	
+	@DeleteMapping("transactions/{id}")
+	public boolean delete(@PathVariable("id") int id) {
+		if(service.delete(id)) {
+			return true;
+		}
+		return false;
 	}
 }
--- BudgetTracker/src/main/java/com/skilldistillery/budgets/services/TransactionServiceImpl.java
+++ BudgetTracker/src/main/java/com/skilldistillery/budgets/services/TransactionServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.budgets.entities.Category;
 import com.skilldistillery.budgets.entities.Transaction;
 import com.skilldistillery.budgets.repositories.TransactionRepository;
 
@@ -32,16 +33,37 @@ public class TransactionServiceImpl implements TransactionService {
 
 	@Override
 	public Transaction create(Transaction transaction) {
-		return null;
+		Category category = new Category();
+		category.setId(1);
+		transaction.setCategory(category);
+		Transaction createdTransaction = repo.saveAndFlush(transaction);
+		return createdTransaction;
 	}
 
 	@Override
 	public Transaction update(int id, Transaction transaction) {
+		Optional<Transaction> foundTransaction = repo.findById(id);
+		Transaction newTransaction = null;
+		if (foundTransaction.isPresent()) {
+			newTransaction = foundTransaction.get();
+			newTransaction.setType(transaction.getType());
+			newTransaction.setAmount(transaction.getAmount());
+			newTransaction.setDescription(transaction.getDescription());
+			repo.saveAndFlush(newTransaction);
+			return newTransaction;
+		}
 		return null;
 	}
 
 	@Override
 	public boolean delete(int id) {
+		Optional<Transaction> optTransaction = repo.findById(id);
+		Transaction foundTransaction = null;
+		if (optTransaction.isPresent()) {
+			foundTransaction = optTransaction.get();
+			repo.delete(foundTransaction);
+			return true;
+		}
 		return false;
 	}
 
