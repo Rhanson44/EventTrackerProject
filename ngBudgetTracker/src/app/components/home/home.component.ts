b6e8c7734fc185383ae34ceb004e96d9917a3f00
Ryan Hanson <hansonrj8322@gmail.com>
2024-09-20 15:35:19 -0600

    API/Front-end smoketest

--- ngBudgetTracker/src/app/components/home/home.component.ts
+++ ngBudgetTracker/src/app/components/home/home.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Transaction } from '../../models/transaction';
+import { TransactionService } from './../../services/transaction.service';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  transactions: Transaction[] = [];
+
+  constructor(private transactionService: TransactionService) { }
+
+  ngOnInit(): void {
+    this.reloadTransactions();
+  }
+
+  reloadTransactions() : void {
+    this.transactionService.index().subscribe({
+      next: (transactionList) => {
+        this.transactions = transactionList;
+      },
+      error: (fail) => {
+        console.error('TransactionComponent.reloadCaves: error retreiving transactionlist');
+        console.error(fail);}
+    })
+  }
 }
--- ngBudgetTracker/src/app/services/transaction.service.ts
+++ ngBudgetTracker/src/app/services/transaction.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Transaction } from '../models/transaction';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
 
-  constructor() { }
+  url = environment.baseUrl + "api/transactions";
+
+  constructor(private http: HttpClient) { }
+
+  index(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(()=>new Error('TransactionService.index(): error retreiving transactions: '));
+      })
+    );
+  }
 }
