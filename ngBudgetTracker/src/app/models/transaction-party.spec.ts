c8dbc009cb3dede9c205fa8259ab1e17e015b3a8
Ryan Hanson <hansonrj8322@gmail.com>
2024-09-23 08:44:25 -0600

    create additional models

---
+++ ngBudgetTracker/src/app/models/transaction-party.spec.ts
@@ -0,0 +1,7 @@
+import { TransactionParty } from './transaction-party';
+
+describe('TransactionParty', () => {
+  it('should create an instance', () => {
+    expect(new TransactionParty()).toBeTruthy();
+  });
+});
